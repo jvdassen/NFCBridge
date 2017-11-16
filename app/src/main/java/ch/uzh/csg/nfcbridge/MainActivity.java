1634bc639163730313ec31adda5df2c0c85e645f
Jan von der Assen <jan.vda@gmx.ch>
2017-11-16 14:29:12 +0100

    Switch to PWA or Google Chrome if a button is clicked.

--- app/src/main/java/ch/uzh/csg/nfcbridge/MainActivity.java
+++ app/src/main/java/ch/uzh/csg/nfcbridge/MainActivity.java
@@ -1,13 +1,37 @@
 package ch.uzh.csg.nfcbridge;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button submitBtn;
+    EditText textAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textAmount = (EditText) findViewById(R.id.transaction_amount);
+        submitBtn = (Button) findViewById(R.id.submit_btn);
+
+        submitBtn.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View view){
+                        goToBazoPaymentPage(textAmount.getText().toString());
+                    }
+                }
+        );
+
     }
+    public void goToBazoPaymentPage(String amount) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://bazopay2.surge.sh/#/auth/user/send/?paymentinfo=bazo%3A0003332221144%3Famount%3D" + amount));
+        startActivity(browserIntent);
+    }
+
 }
