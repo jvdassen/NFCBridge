2e78d7b157bdb6ba5f2036caf9f97870a58ef9a9
Jan von der Assen <jan.vda@gmx.ch>
2017-11-16 16:29:27 +0100

    Add a view to display NDEF messages received as intent

---
+++ app/src/main/java/ch/uzh/csg/nfcbridge/NDEFReceived.java
@@ -0,0 +1,13 @@
+package ch.uzh.csg.nfcbridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NDEFReceived extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ndefreceived);
+    }
+}
