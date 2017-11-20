2d1132708ae197dba7c4cd069e9ba39ffc8857e1
Jan von der Assen <jan.vda@gmx.ch>
2017-11-20 20:55:43 +0100

    Show parameter received as intent link on Main Activity.

--- app/src/main/java/ch/uzh/csg/nfcbridge/MainActivity.java
+++ app/src/main/java/ch/uzh/csg/nfcbridge/MainActivity.java
@@ -2,14 +2,19 @@ package ch.uzh.csg.nfcbridge;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    Button submitBtn;
+    Button submitBtn, pushBtn;
     EditText textAmount;
     EditText textAddress;
     EditText textPOSId;
@@ -24,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         textPOSId = (EditText) findViewById(R.id.posid);
 
         submitBtn = (Button) findViewById(R.id.submit_btn);
+        pushBtn = (Button) findViewById(R.id.push_btn);
+
 
         submitBtn.setOnClickListener(
                 new View.OnClickListener(){
@@ -35,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
                                 );
                         goToBazoPaymentPage(encodedTransaction);
 
+                    }
+                }
+        );
+        pushBtn.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View view){
+                        String encodedTransaction = encodeAsBazoTransactionURI(
+                                textAmount.getText().toString(),
+                                textAddress.getText().toString(),
+                                textPOSId.getText().toString()
+                        );
+                        Uri data = getIntent().getData();
+                        List<String> params = data.getPathSegments();
+                        String first = params.get(0); // "status"
+                        System.out.println("first parameter" + first);
+                        textAddress.setText(first);
                     }
                 }
         );
@@ -53,8 +76,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         result += "paymentinfo=" + "bazo:" + bazoaddress + "?amount=" + amount;
-        System.out.println("trying to access: " + result);
-        System.out.println("encoded: " + Uri.encode(result));
         return result;
     }
 
