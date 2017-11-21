075463c7f68c2d92e3c7ff75fb769db12f99241b
Jan von der Assen <jan.vda@gmx.ch>
2017-11-21 17:37:49 +0100

    Add unit tests for Bazo encoding/decoding

--- app/src/test/java/ch/uzh/csg/nfcbridge/BazoURISchemeTest.java
+++ app/src/test/java/ch/uzh/csg/nfcbridge/BazoURISchemeTest.java
@@ -1,12 +1,48 @@
 package ch.uzh.csg.nfcbridge;
 
+import junit.framework.Assert;
+
 import org.junit.Before;
-import ch.uzh.csg.nfcbridge.BazoURIScheme;
-import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 
-public class BazoURISchemeTestTest {
+public class BazoURISchemeTest {
+    String amount, bazoaddress, optionalPOSID, allParams, onlyAddressAndAmount, onlyAddress, onlyAmount, onlyPOSID;
+
     @Before
     public void setUp() throws Exception {
+        amount = "3.5";
+        bazoaddress = "2B4M98CAJ2ND1T54E89AS4598JD32KWU8A";
+        optionalPOSID = "133";
+        allParams = "https://bazopay2.surge.sh/#/auth/user/send?posid=133&paymentinfo=bazo:2B4M98CAJ2ND1T54E89AS4598JD32KWU8A?amount=3.5";
+        onlyAddressAndAmount = "https://bazopay2.surge.sh/#/auth/user/send?paymentinfo=bazo:2B4M98CAJ2ND1T54E89AS4598JD32KWU8A?amount=3.5";
+        onlyAddress = "https://bazopay2.surge.sh/#/auth/user/send?paymentinfo=bazo:2B4M98CAJ2ND1T54E89AS4598JD32KWU8A";
+        //onlyAmount = "https://bazopay2.surge.sh/#/auth/user/send?paymentinfo=bazo:2B4M98CAJ2ND1T54E89AS4598JD32KWU8A?amount=3.5";
+        onlyPOSID =  "https://bazopay2.surge.sh/#/auth/user/send?posid=133";
+    }
+    @Test
+    public void completeEcodingTest() {
+        String encodedAllParams = BazoURIScheme.encodeAsBazoTransactionURI(bazoaddress, amount, optionalPOSID);
+        assertTrue(encodedAllParams.equals(allParams));
+    }
+    @Test
+    public void partialEncodingTest() {
+        String encodedOnlyAddressAndAmount = BazoURIScheme.encodeAsBazoTransactionURI(bazoaddress, amount, "");
+        assertTrue(encodedOnlyAddressAndAmount.equals(onlyAddressAndAmount));
+
+        String encodedOnlyAddress = BazoURIScheme.encodeAsBazoTransactionURI(bazoaddress, "", "");
+        assertTrue(encodedOnlyAddress.equals(onlyAddress));
+    }
+    @Test(expected = UnsupportedOperationException.class)
+    public void unsupportedEncodingTest() {
+        String encodedOnlyPOSID = BazoURIScheme.encodeAsBazoTransactionURI( "", "", "133");
+
+    }
+    @Test
+    public void decodingTest() {
+        String encodedAllParams = BazoURIScheme.encodeAsBazoTransactionURI(bazoaddress, amount, optionalPOSID);
+        BazoURIScheme.decodeFromTransactionURI(encodedAllParams);
     }
 }
